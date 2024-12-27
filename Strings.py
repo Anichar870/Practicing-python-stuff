8f003d0bbf24b5b7fa2017f63d917642faafb659
Anichar <ponglijamir870@gmail.com>
2024-12-28 00:43:49 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,38 @@
+mylist = [10, 20, 30]
+mylist[1]=25
+
+mylist.extend([50, 60, 70,80])
+mylist[3] = 46
+mylist [5]= 69
+
+mylist.extend([50, 60, 79, 79, 50])
+
+mylist.append(599)
+
+mylist = mylist + [60, 79, 54]
+
+for item in [50, 69, 39]: #add ':' in the end for item thing
+  mylist.append(item)
+  
+for hi in [60, 70]:
+    mylist.append(hi)
+    
+mylist.extend([60, 70, 80])
+mylist.extend([40, 50, 80, 60])
+
+mylist.extend([50, 70, 89])
+
+mylist[13]= 70
+mylist[12]= 68
+
+mylist.append(89)
+
+mylist = mylist + [80,79,60]
+
+for sigma in [70,69,50]:
+  mylist.append(sigma)
+  
+  for mewing in [80]:
+      mylist.append(mewing)
+
+print(mylist)
