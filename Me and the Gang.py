9d251e59a1f005f13d8d080a52b796678ec40454
Anichar <ponglijamir870@gmail.com>
2025-01-07 00:37:50 +0530

    Add files via upload

---
+++ Me and the Gang.py
@@ -0,0 +1,8 @@
+Gang = ["Pongli","Akai","Binchang","Hito", "Zhahuve","Lipok","Peram", "Anguvika", "Sentilog"]
+
+if "Pongli" in Gang:
+  print("Pongli is in the gang")
+elif "Anguvika" in Gang:
+  print("Bro is a Rizzler")
+else:
+  print("No one in Gang") 
