91348c0fcf8246213bf01b70b3cf76898e760df3
Anichar <ponglijamir870@gmail.com>
2025-02-16 10:24:47 -0800

    Add files via upload

---
+++ Using While and In.py
@@ -0,0 +1,25 @@
+count=0
+while count < 5:
+    print(count)
+    count += 1
+    
+
+num=[1,4.6,7]
+print(3 in num)
+print(1 in num)
+
+for char in "hi":
+   print(char)
+   
+   
+term = "I love python"
+print(term)
+if "I" in term:
+    print("It is there")
+    
+    
+    
+Love = "I love her so much"
+print(Love)
+if "her" in Love:
+    print("You are always on my mind")
